1c0aed0605dd92ee5fcac49a0baf4b361e957843
danilo.meneghel <danilo.meneghel@gmail.com>
2019-11-28 01:45:57 -0300

    Upload files

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useState, Fragment, useEffect } from 'react'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
+import UserTable from './tables/UserTable'
+import axios from 'axios';
+
+const App = () => {
+	// Data
+	/*const usersData = [
+		{ id: 1, name: 'Tania', username: 'floppydiskette' },
+		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
+		{ id: 3, name: 'Ben', username: 'benisphere' },
+	]*/
+	
+	const [users, setUsers] = useState({});
+	//const [showLoading, setShowLoading] = useState(true);
+	const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+	useEffect(() => {
+	//setShowLoading(false);
+	const fetchData = async () => {
+	  const result = await axios(apiUrl);
+	  setUsers(result.data);
+	  //setShowLoading(false);
+	};
+
+	fetchData();
+	}, []);
+
+	const initialFormState = { id: null, name: '', username: '' }
+
+	// Setting state
+	//const [ users, setUsers ] = useState(data)
+	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	const [ editing, setEditing ] = useState(false)
+
+	// CRUD operations
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setEditing(false)
+
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false)
+
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditing(true)
+
+		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+	}
+
+	return (
+		<div className="container">
+			<h1>CRUD React with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{editing ? (
+						<Fragment>
+							<h2>Edit user</h2>
+							<EditUserForm
+								editing={editing}
+								setEditing={setEditing}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+						</Fragment>
+					) : (
+						<Fragment>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</Fragment>
+					)}
+				</div>
+			</div>
+			<br />
+			<div className="flex-row">
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
